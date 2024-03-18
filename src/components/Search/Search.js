957be2730e02831301388ba127916ec70cc173d9
Minu Susan John <minususanj@gmail.com>
2024-03-18 14:45:56 +0530

    indentation and extra space removal

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,8 +3,6 @@ import { AsyncPaginate } from "react-select-async-paginate";
 import { geoApiOptions, GEO_API_URL } from "../../Api";
 import './Search.css';
 
-
-
 const Search = ({onSearchChange}) => {
 	const [search, setSearch] = useState(null);
 
@@ -52,27 +50,25 @@ const Search = ({onSearchChange}) => {
 	}
 
 	return (
-		<>
-			<div className='search-container'>
-					<div className='search-header'>
-						<p className='search-header-content'>Weather App</p>
-					</div>
-					<AsyncPaginate
-							placeholder='Enter City Name'
-							debounceTimeout={600}
-							value={search}
-							onChange={handleOnChange}
-							loadOptions={loadOptions}
-					/>
-					<div className='divider-div'>
-						<hr className='divider-line' />
-						<span className='divider-text'>or</span>
-					</div>
-					<div className='device-location-div'>
-						<button className='device-location-button'>Get Device Location</button>
-					</div>
-			</div>
-		</>
+		<div className='search-container'>
+				<div className='search-header'>
+					<p className='search-header-content'>Weather App</p>
+				</div>
+				<AsyncPaginate
+						placeholder='Enter City Name'
+						debounceTimeout={600}
+						value={search}
+						onChange={handleOnChange}
+						loadOptions={loadOptions}
+				/>
+				<div className='divider-div'>
+					<hr className='divider-line' />
+					<span className='divider-text'>or</span>
+				</div>
+				<div className='device-location-div'>
+					<button className='device-location-button'>Get Device Location</button>
+				</div>
+		</div>
 	)
 }
 

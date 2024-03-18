e167f1cd147f0b2e04a9f2f4c8d979fa407090af
Minu Susan John <minususanj@gmail.com>
2024-03-18 10:53:02 +0530

    changed indentation

--- src/components/Current-Weather/CurrentWeather.js
+++ src/components/Current-Weather/CurrentWeather.js
@@ -1,44 +1,44 @@
 import './CurrentWeather.css';
 
 const CurrentWeather = ({data, changeShow}) => {
-    // destructuring the data
-    const {city, weather, main:{humidity, temp, feels_like}} = data;
-    const {description, icon} = weather[0];
-    return (
-			<div className='weather'>
-				<div className='top'>
-					<img className='top-left-arrow' src='left-arrow.png' alt='left-arrow' onClick={() => changeShow()} />
-					<span className='top-text'>Weather App</span>					
-						{/* <img alt='weather' className='weather-icon' src={`https://openweathermap.org/img/wn/${icon}@2x.png`} /> */}
-				</div>
-				<div className='middle'>						
-					{/* <p className='temperature'>{Math.round(temp)} °C</p> */}
-					<img alt='weather' className='weather-icon' src={`https://openweathermap.org/img/wn/${icon}@2x.png`} />
-					<p className='temperature'>{Math.round(temp)}°C</p>
-					<p className='description'>{description}</p>
-					<div className='city-details'>
-						<img className='location-img' src='location-icon.png' alt='location-icon' />
-						<p className='city-name'>{city}</p>
-					</div>
-				</div>
-				<div className='bottom'>
-					<div className='bottom-left'>
-						<img alt='feels-like' src='feels-like.png' className='feels-like-img' />
-						<div className='feels-like'>
-							<p className='feels-like-top'>{Math.round(feels_like)}°C</p>
-							<p className='feels-like-bottom'>Feels Like</p>
-						</div>
-					</div>
-					<div className='bottom-right'>
-						<img alt='humidity' src='humidity.png' className='humidity-img' />
-							<div className='humidity'>
-								<p className='humidity-top'>{humidity}%</p>
-								<p className='humidity-bottom'>Humididty</p>
-							</div>
-						</div>
+	// destructuring the data
+	const {city, weather, main:{humidity, temp, feels_like}} = data;
+	const {description, icon} = weather[0];
+	return (
+		<div className='weather'>
+			<div className='top'>
+				<img className='top-left-arrow' src='left-arrow.png' alt='left-arrow' onClick={() => changeShow()} />
+				<span className='top-text'>Weather App</span>					
+					{/* <img alt='weather' className='weather-icon' src={`https://openweathermap.org/img/wn/${icon}@2x.png`} /> */}
+			</div>
+			<div className='middle'>						
+				{/* <p className='temperature'>{Math.round(temp)} °C</p> */}
+				<img alt='weather' className='weather-icon' src={`https://openweathermap.org/img/wn/${icon}@2x.png`} />
+				<p className='temperature'>{Math.round(temp)}°C</p>
+				<p className='description'>{description}</p>
+				<div className='city-details'>
+					<img className='location-img' src='location-icon.png' alt='location-icon' />
+					<p className='city-name'>{city}</p>
 				</div>
 			</div>
-    )
+			<div className='bottom'>
+				<div className='bottom-left'>
+					<img alt='feels-like' src='feels-like.png' className='feels-like-img' />
+					<div className='feels-like'>
+						<p className='feels-like-top'>{Math.round(feels_like)}°C</p>
+						<p className='feels-like-bottom'>Feels Like</p>
+					</div>
+				</div>
+				<div className='bottom-right'>
+					<img alt='humidity' src='humidity.png' className='humidity-img' />
+						<div className='humidity'>
+							<p className='humidity-top'>{humidity}%</p>
+							<p className='humidity-bottom'>Humididty</p>
+						</div>
+					</div>
+			</div>
+		</div>
+	)
 }
 
 export default CurrentWeather
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -6,74 +6,74 @@ import './Search.css';
 
 
 const Search = ({onSearchChange}) => {
-    const [search, setSearch] = useState(null);
+	const [search, setSearch] = useState(null);
 
-    // called in AsyncPaginate Search component for passing selected input
-    function handleOnChange(searchData) {
-        setSearch(searchData);
-        onSearchChange(searchData);
-    }
-   
-    // called in AsyncPaginate Search component for loading options when typed.. we have to return options we fetch from https://rapidapi.com/wirefreethought/api/geodb-cities/
-    async function loadOptions(inputValue) {
-        try {
-						if(inputValue !== '') {
-							const response = await fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions);
-							const result = await response.json();
-							// console.log('result', result);
-							// console.log('search', search);
-							return {
-									options: result.data.map(city => {
-											return {
-													value: `${city.latitude} ${city.longitude}`,
-													label: `${city.city}, ${city.country}`,
-											}
-									})
-							}
-
-						} else {
-							return {options: [
-								{
-									value: ``,
-									label: `Please type the city name`,
+	// called in AsyncPaginate Search component for passing selected input
+	function handleOnChange(searchData) {
+		setSearch(searchData);
+		onSearchChange(searchData);
+	}
+	
+	// called in AsyncPaginate Search component for loading options when typed.. we have to return options we fetch from https://rapidapi.com/wirefreethought/api/geodb-cities/
+	async function loadOptions(inputValue) {
+		try {
+			if(inputValue !== '') {
+				const response = await fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions);
+				const result = await response.json();
+				// console.log('result', result);
+				// console.log('search', search);
+				return {
+						options: result.data.map(city => {
+								return {
+										value: `${city.latitude} ${city.longitude}`,
+										label: `${city.city}, ${city.country}`,
 								}
-							]}
-						}
-            
-        } catch (error) {
-            console.error(error);
-						return {options: [
-							{
-								value: ``,
-								label: `Invalid City Name`,
-							}
-						]}
-        }
-    }
+						})
+				}
 
-    return (
-        <>
-            <div className='search-container'>
-								<div className='search-header'>
-									<p className='search-header-content'>Weather App</p>
-								</div>
-                <AsyncPaginate
-                    placeholder='Enter City Name'
-                    debounceTimeout={600}
-                    value={search}
-                    onChange={handleOnChange}
-                    loadOptions={loadOptions}
-                />
-								<div className='divider-div'>
-									<hr className='divider-line' />
-									<span className='divider-text'>or</span>
-								</div>
-								<div className='device-location-div'>
-									<button className='device-location-button'>Get Device Location</button>
-								</div>
-            </div>
-        </>
-    )
+			} else {
+				return {options: [
+					{
+						value: ``,
+						label: `Please type the city name`,
+					}
+				]}
+			}
+			
+		} catch (error) {
+			console.error(error);
+			return {options: [
+				{
+					value: ``,
+					label: `Invalid City Name`,
+				}
+			]}
+		}
+	}
+
+	return (
+		<>
+			<div className='search-container'>
+					<div className='search-header'>
+						<p className='search-header-content'>Weather App</p>
+					</div>
+					<AsyncPaginate
+							placeholder='Enter City Name'
+							debounceTimeout={600}
+							value={search}
+							onChange={handleOnChange}
+							loadOptions={loadOptions}
+					/>
+					<div className='divider-div'>
+						<hr className='divider-line' />
+						<span className='divider-text'>or</span>
+					</div>
+					<div className='device-location-div'>
+						<button className='device-location-button'>Get Device Location</button>
+					</div>
+			</div>
+		</>
+	)
 }
 
 export default Search;
